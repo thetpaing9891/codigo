3bce0d3b58369714c6b31192f7cf178e62d59b2c
Thet Paing Soe <thetpaing.soe@kbzbank.com>
2022-05-26 14:51:09 +0630

    Update Readme

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,16 +1,31 @@
+import { motion } from "framer-motion";
 import Footer from "./footer";
 import Header from "./header";
 
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
 interface LayoutProps {
   children: React.ReactNode;
 }
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div id="wrapper">
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <motion.main
+      variants={variants} // Pass the variant object into Framer Motion
+      initial="hidden" // Set the initial state to variants.hidden
+      animate="enter" // Animated state to variants.enter
+      exit="exit" // Exit state (used later) to variants.exit
+      transition={{ type: "linear" }} // Set the transition to linear
+      className=""
+    >
+      <div id="wrapper">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </motion.main>
   );
 };
 
